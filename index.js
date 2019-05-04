e2dc0e4f5243662365f0175853dba36775940b2d
ShankarNayana <nayanas.shankar@gmail.com>
2019-05-04 15:27:12 +0530

    commit on 4th of May

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('external js file')
+
+var a = 10;
+var A = 20;
+var a12 = 23;
+
+// variable name/placeholder can not start with a number or a special character. _ is an exception
+var _hello = 40;
+
+// to represent a constant variable use all uppercase letters
+var PI = 3.14;
+
+// use either _ or uppercase as first letter or the word for variable name
+var user_name = 'nayana';
+var userName = 'shankar'; // java script is dynamic that is you need not define the data type before using it,so var can contain number and characters
+
+console.log(a, A);
+console.log(A);
+console.log(a12);
+console.log(_hello);
+console.log(PI);
+console.log(user_name);
+console.log(userName);n
